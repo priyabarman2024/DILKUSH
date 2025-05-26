0446b934f217685f3bb6cf529a91bd1fdbbf709a
Priya Barman <priyamanit2024@gmail.com>
2025-05-26 14:08:31 +0530

    Delete backend/config/db.js

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB= async()=>{
-    await mongoose.connect('mongodb+srv://priyamanit2024:Toffee2024@cluster0.tforwac.mongodb.net/DILKUSH')
-    .then(()=>console.log("DB Connected"));
-}
